48478facc7c987ac36f59b12e0900113b5d7d13c
Bhavya Panchal <panchalbhavya2210@gmail.com>
2025-05-24 19:06:55 +0530

    logs k

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
   import { onMount } from "svelte";
   import Home from "../pages/Home.svelte";
+   console.log(import.meta.env.VITE_PROJECT_ID)
+  console.log(import.meta.env.VITE_ENDPOINT)
+  console.log(import.meta.env.VITE_BUCKET_ID)
+  console.log(import.meta.env.VITE_DB_ID)
+  console.log(import.meta.env.VITE_COLLECTION_ID)
+  console.log(import.meta.env.VITE_ADMIN_ID)
 </script>
 
 <main>
